cc8f98d3ee39033352170893c61d12b43235b031
gokhulm <tbnrgokhul@gmail.com>
2023-10-16 18:47:55 +0530

    new

---
+++ Lab Programs/Lab 5 16-10-23/4. upper_tri.cpp
@@ -0,0 +1,32 @@
+//Program to print upper triangular matrix
+#include<stdio.h>
+int main()
+{
+	int n,i,v,j,m;
+	printf("Enter value of m ");
+	scanf("%d",&m);
+	printf("Enter value of n ");
+	scanf("%d",&n);
+	int a[m][n];
+	for (i=0;i<n;i++)
+	{
+		for (j=0;j<n;j++)
+		{
+			printf("Enter value of a[%d][%d] ",i,j);
+			scanf("%d",&v);
+			a[i][j]=v;
+		}
+	}
+	for (i=0;i<n;i++)
+	{
+		for (j=0;j<n;j++)
+		{
+			if (i>j)
+				printf("0 ");
+			else
+				printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}	
+	return 0;
+}
